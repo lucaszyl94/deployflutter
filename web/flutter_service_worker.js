7ac661b159cd36054d8d89613fa18f2a2a7b7a21
lucaszyl94 <you@example.com>
2021-08-24 22:14:56 +0200

    last

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "170423c721b66a3c1ac19c11bf96ae80",
-"index.html": "21518fce4526a64ad19a7d299a4f431c",
-"/": "21518fce4526a64ad19a7d299a4f431c",
-"main.dart.js": "c23c3540a0dccbbe76443c97c942c6b7",
+"index.html": "355dfcde0df7e6922e2f250b83abf458",
+"/": "355dfcde0df7e6922e2f250b83abf458",
+"main.dart.js": "210729053901da54f0bebc09051d2820",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
